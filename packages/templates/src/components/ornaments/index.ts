ab0cc548c06a72fd7fd10fe69c949aa4af64a4cd
Kelvin Prasetya <kelvin.prasetya@email.com>
2026-08-12 22:33:37 +0700

    feat(templates): barrel export for ornaments components

---
+++ packages/templates/src/components/ornaments/index.ts
@@ -0,0 +1,4 @@
+export { CornerOrnament } from "./corner-ornament";
+export type { OrnamentCorner, OrnamentVariant } from "./corner-ornament";
+export { PaperTexture } from "./paper-texture";
+export { PortraitFrame } from "./portrait-frame";
